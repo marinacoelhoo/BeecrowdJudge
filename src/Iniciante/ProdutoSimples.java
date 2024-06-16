1f0bdfbf27c958363fe9fd29284afa017168ac1e
Mari <marinacoelho01@hotmail.com>
2024-06-16 00:35:13 -0300

    Exercício de Produto Simples

--- src/Iniciante/ProdutoSimples.java
+++ src/Iniciante/ProdutoSimples.java
@@ -1,2 +1,22 @@
-package Iniciante;public class ProdutoSimples {
+package Iniciante;
+
+import java.util.Scanner;
+/*
+--Entrada--
+O arquivo de entrada contém 2 valores inteiros.
+--Saída--
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade
+Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem:
+“Presentation Error”.
+ */
+public class ProdutoSimples {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int PROD = a * b;
+
+        System.out.println("PROD = " + PROD);
+    }
 }
